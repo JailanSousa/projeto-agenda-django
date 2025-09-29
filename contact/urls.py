860af2307b53b74fcae68c3aabc5b30262bcc44d
JailanSousa <jailann22silvasousa@gmail.com>
2025-09-29 13:37:42 -0300

    initial

--- contact/urls.py
+++ contact/urls.py
@@ -4,5 +4,14 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    path('search/', views.search, name='search'),
     path('', views.index, name='index'),
+    path('contact/create/', views.create, name='create'),
+    # path('contact/<int:contact_id>/update/', views.contact, name='contact'),
+    # path('contact/<int:contact_id>/delet/', views.contact, name='contact'),
+
+
+
+
 ]
