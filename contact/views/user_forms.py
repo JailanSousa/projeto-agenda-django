4b4a0b6854b68de75182fa23c95637e1f9efd9df
JailanSousa <jailann22silvasousa@gmail.com>
2025-10-04 14:19:03 -0300

    initial

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,15 +1,26 @@
-from django.shortcuts import render
-from contact.forms import RegisterForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+
+from contact.forms import RegisterForm, RegisterUpdateForm
 
 def register(request):
 
     form = RegisterForm()
+
+    # messages.info(request, 'Um texto qualquer')
+    # messages.success(request, 'Um texto qualquer')
+    # messages.warning(request, 'Um texto qualquer')
+    # messages.error(request, 'Um texto qualquer')
 
     if request.method == 'POST':
         form = RegisterForm(request.POST)
 
         if form.is_valid():
             form.save()
+            messages.success(request, 'Usuário registrado')
+            return redirect('contact:index')
 
     return render(
         request,
@@ -18,3 +29,62 @@ def register(request):
             'form': form
         }
     )
+
+def login_view(request):
+
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'Logado com sucesso!')
+
+            return redirect('contact:index')
+       
+        messages.error(request, 'Login inválido')
+
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form
+        }
+    )
+
+def user_update(request):
+
+    form = RegisterUpdateForm(instance=request.user)
+
+    if request.method != 'POST':
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+    
+    form = RegisterUpdateForm(data=request.POST,
+                              instance=request.user)
+    
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form.save()
+    auth.login(request, request.user)
+
+    return redirect('contact:user_update')
+
+def logout_view(request):
+    auth.logout(request)
+
+    return redirect('contact:login')
